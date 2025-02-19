d140c1b8311a07a5863f422d767cf689ccc85d5e
TannazC <TannazHChowdhury@gmail.com>
2025-02-18 22:14:33 -0500

    Update Pythagorean Triples.py

--- Pythagorean Triples.py
+++ Pythagorean Triples.py
@@ -1,6 +1,7 @@
 #If all three sides of a right-angled triangle are integers, the set of these three numbers is known as a
 #Pythagorean triple. For example, (3, 4, 5) is a Pythagorean triple because 32+42=52 Write a
 #program that displays all Pythagorean triples where the hypotenuse is no larger than 500 units
+
 Pytriple=set()
 for a in range(1,501):
     for b in range(1,501):
